d173358540a310adc4ee82fcbbd272b265435ca0
HugoSRagain <raguhugo37@gmail.com>
2026-05-31 11:03:10 +0200

    Prepare June 2026 SMIC comparison analysis

---
+++ analysis/smic_june_2026_comparison.py
@@ -0,0 +1,8 @@
+"""
+Comparison script for the June 2026 SMIC / RGDU update.
+
+Purpose:
+- compare May 2026 reference dataset with the future June 2026 dataset;
+- identify changes in gross wage, employer cost, net wage and RGDU;
+- prepare charts on the effect of frozen relief parameters.
+"""
